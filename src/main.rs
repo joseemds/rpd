4f9b760a2e82b1858e0ff2318b60a19085007915
José Eduardo Monteiro <mds.josee@gmail.com>
2022-09-24 18:06:31 -0300

    add Ctrl+C and Esc handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-use clap::{App, Arg, Parser, SubCommand};
-use inquire::{list_option::ListOption, MultiSelect, Text};
+use clap::{App, Parser, SubCommand};
+use inquire::{list_option::ListOption, InquireError, MultiSelect, Text};
+
+/// Register your automatic thoughts
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[clap(short, long, value_parser)]
+    name: String,
+
+    /// Number of times to greet
+    #[clap(short, long, value_parser, default_value_t = 1)]
+    count: u8,
+}
+
+// #[derive(Debug)]
+// struct Thought {
+//     feeling: String,
+//     situation: String,
+//     thought: String,
+//     intensity: i32
+// }
+
 fn main() {
     let app = App::new("rpd")
         .author("joseemds")
@@ -9,19 +31,53 @@ fn main() {
         .get_matches();
 
     println!("App: {:?}", app);
-    match app.subcommand(){
-        Some(("register", _))
-        | None => {
+    // let args = Args::parse();
+    // let _ = Thought {feeling: String::from("bad"), situation: String::from("really bad"), thought: String::from("im a piece of shit"), intensity: 10};
+    match app.subcommand() {
+        Some(("register", _)) | None => {
             let options = vec![
                 ListOption::new(0, &"sad"),
                 ListOption::new(1, &"angry"),
                 ListOption::new(2, &"frustated"),
             ];
-            let feeling = MultiSelect::new("What are you feeling now", options).prompt();
-            let situation = Text::new("What happened that made you feel like this?").prompt();
-            let thought = Text::new("What do you think about this?").prompt();
-            let intensity =
-                Text::new("How intensive is that feeling in a scale of [1-10]").prompt();
+
+            let feeling = MultiSelect::new("What are you feeling now", options)
+                .prompt()
+                .map_err(|err| match err {
+                    InquireError::OperationInterrupted | InquireError::OperationCanceled => {
+                        std::process::exit(1)
+                    }
+                    err => err,
+                })
+                .unwrap();
+
+            let situation = Text::new("What happened that made you feel like this?")
+                .prompt()
+                .map_err(|err| match err {
+                    InquireError::OperationInterrupted | InquireError::OperationCanceled => {
+                        std::process::exit(1)
+                    }
+                    err => err,
+                })
+                .unwrap();
+            let thought = Text::new("What do you think about this?")
+                .prompt()
+                .map_err(|err| match err {
+                    InquireError::OperationInterrupted | InquireError::OperationCanceled => {
+                        std::process::exit(1)
+                    }
+                    err => err,
+                })
+                .unwrap();
+            let intensity = Text::new("How intensive is that feeling in a scale of [1-10]")
+                .prompt()
+                .map_err(|err| match err {
+                    InquireError::OperationInterrupted | InquireError::OperationCanceled => {
+                        std::process::exit(1)
+                    }
+                    err => err,
+                })
+                .unwrap();
         }
         _ => {
             panic!("Unexpected Command")
